2983100f1ef5608d60d089e52b28d41febd78e1c
nathalie galvez <nathaliegalvez8@gmail.com>
2025-03-03 00:18:12 +0100

    preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, FunctionTransformer
 from sklearn.impute import SimpleImputer
+from imblearn.over_sampling import SMOTE
 from .constants import CATEGORICAL_COLUMNS, NUMERICAL_COLUMNS
 
 class Preprocessing:
-    def __init__(self, train_data, test_data):
+    def __init__(self, train_data, test_data, target_column):
         self.train_data = train_data
         self.test_data = test_data
+        self.target_column = target_column
 
     def drop_columns(self):
         self.train_data = self.train_data.drop(columns=['Loan_ID'], axis=1)
         self.test_data = self.test_data.drop(columns=['Loan_ID'], axis=1)
 
-    def hanlde_missing_values(self):
+    def handle_missing_values(self):
         imputer = SimpleImputer(strategy='mean')
         imputer.fit_transform(self.train_data[NUMERICAL_COLUMNS])
         self.test_data[NUMERICAL_COLUMNS] = imputer.transform(self.test_data[NUMERICAL_COLUMNS])
@@ -30,4 +32,27 @@ class Preprocessing:
             self.test_data[col] = le.transform(self.test_data[col])
 
     def transform_data(self):
+        log_transformer = FunctionTransformer(np.log1p, validate=True)
+        log_columns = ["ApplicantIncome", "CoapplicantIncome", "LoanAmount"]
+
+        self.train_data[log_columns] = log_transformer.fit_transform(self.train_data[log_columns])
+
+        self.test_data[log_columns] = log_transformer.transform(self.test_data[log_columns])
+
+    def apply_smote(self):
+        smote = SMOTE(random_state=42)
         
+        X_train = self.train_data.drop(columns=[self.target_column])
+        y_train = self.train_data[self.target_column]
+        
+        X_resampled, y_resampled = smote.fit_resample(X_train, y_train)
+
+        self.train_data = pd.DataFrame(X_resampled, columns=X_train.columns)
+        self.train_data[self.target_column] = y_resampled
+
+    def preprocess(self):
+        self.drop_columns()
+        self.handle_missing_values()
+        self.encode_categorical_columns()
+        self.transform_data()
+        self.apply_smote()
